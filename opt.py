171d165fb6ed24b622df69a00b73a2f584cee0a3
pradeepbadiger09 <pradeepbadiger618@gmail.com>
2025-10-24 00:45:54 +0530

    Update opt.py

--- opt.py
+++ opt.py
@@ -4,5 +4,4 @@ b = float(input("Enter second number: "))
 # Perform and display calculations
 print("\nAddition:", a + b)
 print("Subtraction:", a - b)
-print("Multiplication:", a * b)
-print("Division:", a / b)
+
